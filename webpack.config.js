b063affbc49bb6f7199ada142b88278854cb2bad
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-11-11 22:55:11 +0600

    fix webpack variable naming issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
-    const path = require('path');
+const path = require('path');
 
-    module.exports = {
-        mode: argv.mode || "development",
-        // entry: '.spa/src/App.js',
-        // output: {
-        //     filename: 'bundle.js',
-        //     path: path.resolve(__dirname, 'build'),
-        // },
-        entry: {
-            admin: path.resolve(__dirname, "spa/admin/settings/src/App.jsx"),
-            public: path.resolve(__dirname, "spa/public/shortcodes/src/App.jsx"),
-        },
-        output: {
-            filename: "[name].bundle.js",
-            path: path.resolve(__dirname, "build"),
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env', '@babel/preset-react'],
-                        },
+module.exports = (env, argv) => ({
+    mode: argv.mode || "development",
+
+    entry: {
+        admin: path.resolve(__dirname, "spa/admin/settings/src/App.jsx"),
+        public: path.resolve(__dirname, "spa/public/shortcodes/src/App.jsx"),
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "build"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
                     },
                 },
-                // Add rules for CSS, images, etc., as needed
-            ],
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-        },
-        // Add plugins as needed (e.g., CleanWebpackPlugin, CopyWebpackPlugin)
-    };
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+});
