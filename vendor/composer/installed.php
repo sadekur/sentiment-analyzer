1f79929a0f150978e7cdda996295a686efd3f455
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-11-11 22:35:18 +0600

    add necessery library

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sentiment/analyzer',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '4e016642a75a054a623e17461be6d6b7f7182426',
+        'reference' => '6e36a5c7623a7b29a89b3a9c431b38648a814fe2',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sentiment/analyzer' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '4e016642a75a054a623e17461be6d6b7f7182426',
+            'reference' => '6e36a5c7623a7b29a89b3a9c431b38648a814fe2',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
