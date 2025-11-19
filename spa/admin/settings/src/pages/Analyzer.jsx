d5e3150dc2677824dc9950ce3e1b9b5787bdcf91
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-11-19 23:12:10 +0600

    add ClearCache component to Analyzer page

--- spa/admin/settings/src/pages/Analyzer.jsx
+++ spa/admin/settings/src/pages/Analyzer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import BulkAnalyzer from "./components/BulkAnalyzer";
+import ClearCache from "./components/ClearCache";
 
 const Analyzer = () => {
   const [settings, setSettings] = useState({
@@ -185,21 +186,7 @@ const Analyzer = () => {
       <BulkAnalyzer />
 
       {/* Clear Cache Section */}
-
-      <div className="mt-8 pt-6 border-t border-gray-200">
-        <h2 className="text-lg font-semibold text-gray-700 mb-3">
-          Clear Cache
-        </h2>
-        <p className="text-gray-600">
-          This action will clear the sentiment cache for all posts.
-        </p>
-        <button
-          // onClick={handleClearCache}
-          className="px-6 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-        >
-          Clear Cache
-        </button>
-      </div>
+      <ClearCache />
 
       <div className="mt-8 pt-6 border-t border-gray-200">
         <h2 className="text-lg font-semibold text-gray-700 mb-3">
